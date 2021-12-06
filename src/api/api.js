f0b5da128cf70b9051633d0aa695985baf5330a8
Igor Gladkiy <egunka1988@gmail.com>
2021-12-06 12:35:16 +0200

    update api
    in request string add filters

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ export default class Country {
     }
 
     fetchCountry() {
-        const url = `https://restcountries.com/v2/name/${this.searchQuery}`
+        const url = `https://restcountries.com/v2/name/${this.searchQuery}?fields=name,capital,population,languages,flags`
         return fetch(url).then(r => {return r.json()})
     }
 
